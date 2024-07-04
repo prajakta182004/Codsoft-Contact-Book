7a74d77e65041b6ec268e61f53407f51ee9e4e00
prajakta182004 <prsapkal18@gmail.com>
2024-07-04 21:19:12 +0530

    Add files via upload

---
+++ contact_Book.py
@@ -0,0 +1,31 @@
+#contact_Book.py
+
+names = []
+phone_numbers = []
+
+num = 3
+
+for i in range(num):
+    name = input("Enter Name: ")
+    phone_number = input("Enter Phone Number: ")
+    
+    names.append(name)
+    phone_numbers.append(phone_number)
+
+
+print("\tName\t\t\tPhone Number")
+
+for i in range(num):
+    print(f'\t{names[i]}\t\t\t{phone_numbers[i]}')
+    
+s = input("Enter the Name to search: ")
+if s in names:
+    index = names.index(s)
+    name = name[index] 
+    phone_number = phone_numbers[index] 
+    
+    print(f"Name:{name} , Phone Number:{phone_number}")
+else:
+    print("Name not found!")  
+                 
+    
